35ac408949ca0f3aea1c9d667b76f1b0153aeb56
dimmdimas <mdrdimas1@gmail.com>
2025-05-04 18:26:43 +0700

    fix error vercel

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser';
 import express from 'express'
-import { connect } from './utils/database';
 import router from './routes/api';
+import connect from './utils/database';
 
 
 export async function init() {
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";    
 import env from "./env";
 
-export const connect = async () => {
+const connect = async () => {
     await mongoose.connect(env.DATABASE, {
         dbName: 'Alone'
     });
 
     return Promise.resolve('Database connect')
 }
+
+export default connect;
